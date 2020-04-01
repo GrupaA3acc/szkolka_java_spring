9c16f6b702c569ff8e0a9c6abf1f01ea5d905751
Natalia <nszarek284@gmail.com>
2020-04-01 23:18:22 +0200

    HobbitController

--- src/main/java/pl/accenture/spring_exercise/controller/HobbitController.java
+++ src/main/java/pl/accenture/spring_exercise/controller/HobbitController.java
@@ -1,6 +1,26 @@
 package pl.accenture.spring_exercise.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pl.accenture.spring_exercise.domain.Hobbit;
+import pl.accenture.spring_exercise.repository.HobbitRepository;
+
+@RestController
 public class HobbitController {
+
+    private HobbitRepository hobbitRepository;
+
+    public HobbitController(HobbitRepository hobbitRepository) {
+        this.hobbitRepository = hobbitRepository;
+    }
+
+    @GetMapping("/hobbit/{id}")
+    public Hobbit pokazHobbita(@PathVariable Long id)
+    {
+        return hobbitRepository.findById(id).get();
+    }
 
 
 }
