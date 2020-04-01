ebe228535ad6cdaf08f86f380c101b82b0681a8b
kkaniap <xkaniax@gmail.com>
2020-04-01 17:41:29 +0200

    initial project

---
+++ src/main/java/pl/accenture/spring_exercise/SpringExerciseApplication.java
@@ -0,0 +1,13 @@
+package pl.accenture.spring_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExerciseApplication.class, args);
+    }
+
+}
