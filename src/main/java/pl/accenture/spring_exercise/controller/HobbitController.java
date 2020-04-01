fad6845b4877623fa82ff817966107db505ed4c5
Natalia <natikrak@gmail.com>
2020-04-01 21:50:54 +0200

    data.sql

--- src/main/java/pl/accenture/spring_exercise/controller/HobbitController.java
+++ src/main/java/pl/accenture/spring_exercise/controller/HobbitController.java
@@ -3,9 +3,11 @@ package pl.accenture.spring_exercise.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import pl.accenture.spring_exercise.entity.Hobbit;
 import pl.accenture.spring_exercise.repository.HobbitRepository;
 
+@RestController
 public class HobbitController {
 
     private final HobbitRepository hobbitRepository;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+--INSERT INTO hobbit VALUES (123, 'Frodo', 'Baggins');
+--INSERT INTO hobbit VALUES (10, 'Felek', 'Borsuk');
+--INSERT INTO hobbit VALUES (20, 'Feniks', 'Bazant');
+
+INSERT INTO hobbit (id, name, last_name) VALUES ('1','Frodo', 'Baggins');
